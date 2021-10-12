04051c8fcde9dc21de7ba054be72edce592963ea
Fábio S. Corrêa <fabiosenracorrea@hotmail.com>
2021-10-12 14:47:27 -0300

    event listener hook improved

--- src/hooks/eventListener.ts
+++ src/hooks/eventListener.ts
@@ -1,22 +1,28 @@
 /* eslint-disable consistent-return */
-import { RefObject, useEffect } from 'react';
+import { RefObject, useEffect, useRef } from 'react';
 
-function useEventListener<T extends HTMLElement = HTMLDivElement>(
-  eventName: keyof WindowEventMap,
+function useEventListener<T extends HTMLElement = HTMLDivElement, CustomWindow = WindowEventMap>(
+  eventName: keyof (CustomWindow extends WindowEventMap ? CustomWindow : WindowEventMap),
   handler: (event: Event) => void,
   element: RefObject<T | Window> = { current: window },
 ): void {
+  const handlerCB = useRef(handler);
+
+  useEffect(() => {
+    handlerCB.current = handler;
+  }, [handler]);
+
   useEffect(() => {
     const targetElement = element.current;
 
     if (!targetElement?.addEventListener) return;
 
-    targetElement.addEventListener(eventName, handler);
+    targetElement.addEventListener(eventName as string, handlerCB.current);
 
     return () => {
-      targetElement.removeEventListener(eventName, handler);
+      targetElement.removeEventListener(eventName as string, handlerCB.current);
     };
-  }, [eventName, element, handler]);
+  }, [eventName, element]);
 }
 
 export default useEventListener;
