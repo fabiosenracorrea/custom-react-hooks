90d8c9422b35e580b641f7080881961ff1f75b22
Fábio S. Corrêa <fabiosenracorrea@hotmail.com>
2021-10-12 15:07:52 -0300

    debounce hook improved

--- src/hooks/debounce.ts
+++ src/hooks/debounce.ts
@@ -1,15 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const HALF_SECOND_IN_MIL_SECONDS = 500;
 
 function useDebounce<T>(value: T, delay: number = HALF_SECOND_IN_MIL_SECONDS): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+
   useEffect(() => {
-    const timer = setTimeout(() => setDebouncedValue(value), delay);
+    if (timerRef.current) clearTimeout(timerRef.current);
+
+    timerRef.current = setTimeout(() => {
+      setDebouncedValue(value);
+
+      timerRef.current = null;
+    }, delay);
 
     return () => {
-      clearTimeout(timer);
+      if (timerRef.current) clearTimeout(timerRef.current);
     };
   }, [value, delay]);
 
